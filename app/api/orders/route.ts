cdf2dbef0f582258ad0f89fa9fb8ec1c114c7291
mahdi6788 <mjalalimech@gmail.com>
2025-03-10 14:22:48 +0330

    comment them temporarly

--- app/api/orders/route.ts
+++ app/api/orders/route.ts
@@ -1,7 +1,7 @@
 // RESTful API approach: use API Route Handlers instead of using Server Actions (more Flexible for External Requests)
 
-import clientPromise from "@/app/lib/mongodb";
-import { NextResponse } from "next/server";
+// import clientPromise from "@/app/lib/mongodb";
+// import { NextResponse } from "next/server";
 
 
 
