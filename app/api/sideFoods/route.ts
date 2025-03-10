c7100742c27cadb35ea1862e4cf4d7f5d502d6b6
mahdi6788 <mjalalimech@gmail.com>
2025-03-10 14:40:50 +0330

    Implement PUT and DELETE methods for sideFoods API route

--- app/api/sideFoods/route.ts
+++ app/api/sideFoods/route.ts
@@ -1,6 +1,7 @@
 /// RESTful API approach: use API Route Handlers instead of using Server Actions (more Flexible for External Requests)
-/*
+
 import clientPromise from "@/app/lib/mongodb";
+import { ObjectId } from "mongodb";
 import { NextResponse } from "next/server";
 
 export async function GET() {
@@ -14,7 +15,7 @@ export async function GET() {
     return NextResponse.json(sideFoods);
   } catch (error) {
     return NextResponse.json(
-      { message: "Failed to fetch data" },
+      { message: "Failed to fetch data", error },
       { status: 500 }
     );
   }
@@ -40,9 +41,47 @@ export async function POST(req: Request) {
     });
   } catch (error) {
     return NextResponse.json(
-      { message: "Failed to fetch data" },
+      { message: "Failed to create data", error },
       { status: 500 }
     );
   }
 }
-*/
+
+export async function PUT(req: Request) {
+  const { id, title, description, price, image } = await req.json();
+  try {
+    /// connect to MongoDB
+    const client = await clientPromise;
+    /// select the database
+    const db = client.db("FoodDatabase");
+    /// fetch collections separately
+    const sideFood = await db
+      .collection("sideFoods")
+      .updateOne({ _id: new ObjectId(id) }, { $set: { title, description, price, image } });
+    return NextResponse.json({
+      id: sideFood.upsertedId?.toString(),
+      title,
+      description,
+      price,
+      image,
+    });
+  } catch(error){
+    return NextResponse.json(
+      { message: "Failed to update data", error },
+      { status: 500 }
+    );
+
+  }
+}
+
+export async function DELETE(req: Request) {
+  const { id } = await req.json();
+  try { 
+    /// connect to MongoDB
+    const client = await clientPromise;
+    /// select the database
+    const db = client.db("FoodDatabase");
+    /// fetch collections separately
+    const sideFood = await db.collection("sideFoods").deleteOne({ _id: new ObjectId(id) });
+  }
+}
