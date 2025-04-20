7165214ebcaaa234ab8424c952d5c43982817c61
mahdi6788 <mjalalimech@gmail.com>
2025-04-20 16:29:01 +0330

    return to session

--- app/(pages)/users/admin/layout.tsx
+++ app/(pages)/users/admin/layout.tsx
@@ -4,7 +4,7 @@ import { redirect } from "next/navigation";
 
 export default async function AdminLayout({children}:{children:React.ReactNode}) {
   const session = await auth();
-  if (!session || session.user.role !== "ADMIN") {
+  if (!session || session?.user.role !== "ADMIN") {
     redirect("/login");
   }
 
--- app/(pages)/users/layout.tsx
+++ app/(pages)/users/layout.tsx
@@ -9,7 +9,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   const router = useRouter();
   const { data: session, status } = useSession();
   
-  if (!session?.user) {
+  if (!session) {
     router.push("/login");
     return null;
   }
