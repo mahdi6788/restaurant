c52593788201215bcfc71436bc3d2f9b648fe0ee
mahdi6788 <mjalalimech@gmail.com>
2025-04-23 18:17:03 +0330

    add createdAt to fetchOrderFn as a dependency to trigger useQuery whenever changes

--- app/context/OrderContext.tsx
+++ app/context/OrderContext.tsx
@@ -5,15 +5,15 @@ import toast from "react-hot-toast";
 import { OrderWithItems } from "../types/types";
 import { useDebounce } from "use-debounce";
 
-
-
 interface OrderContextType {
   search: string;
   setSearch: (search: string) => void;
   sortby: string;
   setSortby: (sortby: string) => void;
+  createdAt: string;
+  setCreatedAt: (createdAt: string) => void;
   selectedOrders: OrderWithItems;
-  selectedOrdersLoading:boolean,
+  selectedOrdersLoading: boolean;
 }
 
 const OrderContext = createContext<OrderContextType | undefined>(undefined);
@@ -26,43 +26,46 @@ export default function OrderProvider({
   const [search, setSearch] = useState("");
   const [debouncedSearch] = useDebounce(search, 300); // Debounce search input by 300ms
   const [sortby, setSortby] = useState<string>("createdAt-desc");
+  const [createdAt, setCreatedAt] = useState("All");
 
   const queryClient = useQueryClient();
-
-  /// Get selected orders
-  const fetchOrderFn = async () => {
-    const query = new URLSearchParams({
-      search:debouncedSearch,
-      sortby,
-    }).toString();
-
-    const res = await fetch(`/api/order?${query}`, {
-      method: "GET",
-      headers: { "Content-Type": "application/json" },
-    });
-    if (!res.ok) throw new Error("There is not any order");
-    const orders = await res.json();
-    return orders;
-  };
-
-  const { data: selectedOrders = [], isPending: selectedOrdersLoading } =
+  
+    /// Get selected orders
+    const fetchOrderFn = async () => {
+      const query = new URLSearchParams({
+        search: debouncedSearch,
+        sortby,
+        createdAt,
+      }).toString();
+      
+      const res = await fetch(`/api/order?${query}`, {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+        cache: "no-store",
+      });
+      if (!res.ok) throw new Error("There is not any order");
+      const orders = await res.json();
+      return orders;
+    }
+    
+    const { data: selectedOrders = [], isPending: selectedOrdersLoading } =
     useQuery({
-      queryKey: ["orders", debouncedSearch, sortby],
+      queryKey: ["orders", debouncedSearch, sortby, createdAt], // these are dependencies just like useEffect dep that update the UI when changing.
       queryFn: fetchOrderFn,
     });
-  
-/// DELETE
-  const deleteOrderFn = async (orderItemId: string) => {
-    await fetch("/api/order", {
-      method: "DELETE",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ orderItemId }),
-    });
-  };
-
-  /// Delete order
- useMutation({
-    mutationFn: deleteOrderFn,
+    
+    /// DELETE
+    const deleteOrderFn = async (orderItemId: string) => {
+      await fetch("/api/order", {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ orderItemId }),
+      });
+    };
+    
+    /// Delete order
+    useMutation({
+      mutationFn: deleteOrderFn,
     mutationKey: ["orders"],
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["orders"] });
@@ -73,7 +76,7 @@ export default function OrderProvider({
       toast.error("Failed to remove order");
     },
   });
-
+  
   return (
     <OrderContext.Provider
       value={{
@@ -81,6 +84,8 @@ export default function OrderProvider({
         setSearch,
         sortby,
         setSortby,
+        createdAt,
+        setCreatedAt,
         selectedOrders,
         selectedOrdersLoading,
       }}
