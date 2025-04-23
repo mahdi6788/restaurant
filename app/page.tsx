5cec9df0bb48534aebeaa6595f82c2dd480c7356
mahdi6788 <mjalalimech@gmail.com>
2025-04-23 18:06:44 +0330

    make the header fix at top

--- app/page.tsx
+++ app/page.tsx
@@ -30,51 +30,55 @@ export default function TodayFood() {
       {/* Body */}
       <div className="relative z-10 w-full max-w-7xl sm:px-6 lg:px-8">
         {/* DESKTOP Header Section */}
-        <div className="hidden sm:flex items-center justify-between py-4">
-          {/* User Actions */}
-          <div className="flex items-center gap-4 text-stone-100 font-bold">
-            {session ? (
-              <div className="flex items-center gap-3">
-                {/* Dashboard / Logout */}
-                <Dropdown isOpen={isOpen} setIsOpen={setIsOpen} />
-              </div>
-            ) : (
-              <div className="flex items-center gap-4">
-                <Link
-                  href="/login"
-                  className="flex items-center gap-1 hover:text-green-300"
-                >
-                  LogIn
-                </Link>
-                <Link
-                  href="/register"
-                  className="flex items-center pl-2 hover:text-green-300"
-                >
-                  Register
-                </Link>
-              </div>
-            )}
-            {/* cart */}
-            <button
-              onClick={() => setIsModalOpen(true)}
-              className=" relative p-2 text-white rounded  hover:text-green-300"
-            >
-              <FaShoppingCart size={30} />
-              <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs px-1.5 py-0.5 rounded-full">
-                {cartItemsLoading
-                  ? "..."
-                  : cartItems.reduce((sum, item) => sum + item.quantity, 0) ||
-                    0}
-              </span>
-            </button>
-          </div>
-          {/* Today's Menu Title */}
-          <div className="absolute top-0 right-1/3 text-stone-800 font-bold bg-slate-100 rounded-b-lg p-2 px-16">
-            {todayFoods.length === 0 ? "No food available" : "Today’s Specials"}
+        <div className=" hidden sm:flex fixed top-0 left-28 z-50 w-[calc(100vw-10rem)]">
+          <div className="flex items-center justify-between py-4">
+            {/* User Actions */}
+            <div className="flex items-center gap-4 text-stone-100 font-bold">
+              {session ? (
+                <div className="flex items-center gap-3">
+                  {/* Dashboard / Logout */}
+                  <Dropdown isOpen={isOpen} setIsOpen={setIsOpen} />
+                </div>
+              ) : (
+                <div className="flex items-center gap-4">
+                  <Link
+                    href="/login"
+                    className="flex items-center gap-1 hover:text-green-300"
+                  >
+                    LogIn
+                  </Link>
+                  <Link
+                    href="/register"
+                    className="flex items-center pl-2 hover:text-green-300"
+                  >
+                    Register
+                  </Link>
+                </div>
+              )}
+              {/* cart */}
+              <button
+                onClick={() => setIsModalOpen(true)}
+                className=" relative p-2 text-white rounded  hover:text-green-300"
+              >
+                <FaShoppingCart size={30} />
+                <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs px-1.5 py-0.5 rounded-full">
+                  {cartItemsLoading
+                    ? "..."
+                    : cartItems.reduce((sum, item) => sum + item.quantity, 0) ||
+                      0}
+                </span>
+              </button>
+            </div>
+            {/* Today's Menu Title */}
+            <div className="absolute top-0 right-1/3 text-stone-800 font-bold bg-slate-100 rounded-b-lg p-2 px-16">
+              {todayFoods.length === 0
+                ? "No food available"
+                : "Today’s Specials"}
+            </div>
           </div>
         </div>
         {/* Products */}
-        <div className="mt-20 sm:mt-0 relative z-10 w-full max-w-7xl px-4 sm:px-6 lg:px-8 pb-20 sm:pb-20">
+        <div className="mt-20 relative w-full max-w-7xl px-4 sm:px-6 lg:px-8 pb-20 sm:pb-20">
           <div
             className={
               todayFoods.length === 1
