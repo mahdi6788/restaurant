cbec605c455988b0ae38a9fdcf78b61d41d6ddfd
mahdi6788 <mjalalimech@gmail.com>
2025-04-24 03:51:51 +0330

    responsive

--- app/components/admin/FoodsList.tsx
+++ app/components/admin/FoodsList.tsx
@@ -7,7 +7,6 @@ import { DeleteFood, UpdateFood } from "../ui/buttons";
 import { FoodsListSkeleton } from "../skeleton";
 import { useMenu } from "@/hooks/useMenu";
 
-
 export default function FoodsList() {
   /// fetch foods using hook contaning use query
   const { menu, menuLoading, menuRefetch } = useMenu();
@@ -30,59 +29,53 @@ export default function FoodsList() {
 
   return (
     <div className="mt-1 flow-root">
-      <div className="inline-block min-w-full align-middle">
-        <div className="rounded-lg bg-gray-50 p-2 sm:pt-0">
+      <div className="inline-block sm:bg-stone-50 min-w-full align-middle">
+        <div className="rounded-lg sm:p-2">
           {/* Mobile Version */}
           {/* pointing to hidden in small screen into the parent tag */}
           <div className="sm:hidden">
             {menu?.map((food: MenuItem) => (
               <div
                 key={food.id}
-                className="mb-2 w-full rounded-md bg-white p-4"
+                className="relative bg-stone-100 opacity-85 flex flex-col p-2 rounded-2xl shadow-lg h-96 my-2"
               >
-                <div className="flex items-center justify-between border-b pb-4">
-                  <div>
-                    <div className="flex items-center mb-2 gap-1">
-                      {/* Availibility */}
-                      <div className="flex items-center justify-center gap-1 peer w-full rounded-md py-2 pl-10 text-sm outline-2 placeholder:text-gray-500">
-                        <label htmlFor={`availability-${food.id}`}>
-                          Is Availabile?{" "}
-                        </label>
-                        <input
-                          checked={food.isAvailable ?? false}
-                          onChange={() => handleAvailibility(food)}
-                          type="checkbox"
-                          id={`availability-${food.id}`}
-                          name="availability"
-                        />
-                      </div>
-                      {/* Image */}
-                      <Image
-                        src={
-                          typeof food.imageUrl === "string"
-                            ? food.imageUrl
-                            : "/public/images/logo/LOGO.jpg"
-                        }
-                        alt={food.name || "Food image"}
-                        className="mr-2 rounded-full object-cover"
-                        width={70}
-                        height={70}
-                      />
-                      {/* Category */}
-                      <p>{food.category}</p>
-                      {/* Name */}
-                      <p>{food.name}</p>
-                    </div>
-                    {/* Description */}
-                    <p className="text-sm text-gray-500">{food.description}</p>
-                  </div>
+                {/* Image */}
+                <div className="h-60 w-full relative rounded-xl overflow-hidden">
+                  <Image
+                    src={food.imageUrl ?? "/public/images/logo/LOGO.jpg"}
+                    alt={food.name || "Food image"}
+                    layout="fill"
+                    placeholder="empty"
+                    objectFit="cover"
+                  />
                 </div>
-                {/* Price */}
-                <div className="flex w-full items-center justify-between pt-4">
-                  <div>
-                    <p className="text-xl font-medium">
-                      {formatCurrency(food.price || 0)}
-                    </p>
+
+                {/* Category / Name / Description */}
+                <div className="space-y-1 flex flex-col items-start text-sm rounded-lg border border-gray-800 shadow-md shadow-gray-600 text-white bg-gray-800 p-1 mt-1 w-full">
+                  <div
+                    className={`${food.category === "MainCourse" ? "text-green-500" : "text-yellow-500"}`}
+                  >
+                    {food.category}
+                  </div>
+                  <h2 className="">{food.name}</h2>
+                  <p className="w-full text-sm">{food.description}</p>
+                </div>
+
+                <p className="absolute left-0 bottom-0 font-semibold text-yellow-300 bg-blue-600 px-2 py-1 rounded-bl-2xl rounded-tr-3xl">
+                  AED {food.price}
+                </p>
+                <div className="flex w-full items-center justify-end gap-2 p-1 ">
+                  {/* Availability */}
+                  <div className="flex items-center gap-1">
+                    <label htmlFor={`availability-${food.id}`}>Available</label>
+                    <input
+                      checked={food.isAvailable ?? false}
+                      onChange={() => handleAvailibility(food)}
+                      type="checkbox"
+                      id={`availability-${food.id}`}
+                      name="availability"
+                      className="size-4"
+                    />
                   </div>
                   {/* Update/Delete */}
                   <div className="flex justify-end gap-2">
@@ -93,7 +86,7 @@ export default function FoodsList() {
               </div>
             ))}
           </div>
-          
+
           {/* Desktop Version */}
           {/* when pointing to hidden and sm:table in advance, no need to point the screen size for other tags  */}
           <table className="hidden min-w-full text-gray-900 sm:table">
@@ -126,12 +119,12 @@ export default function FoodsList() {
                   key={food.id}
                   className="w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg"
                 >
-                  {/* Availibility */}
+                  {/* Availability */}
                   <td>
                     <div className="flex items-center gap-6 -ml-6">
                       <div className="flex items-center justify-center gap-1 peer w-full rounded-md py-2 pl-10 text-sm outline-2 placeholder:text-gray-500">
                         <label htmlFor={`availabilityDesktop-${food.id}`}>
-                          Is Availabile?
+                          Available
                         </label>
                         <input
                           checked={food.isAvailable ?? false}
@@ -139,6 +132,7 @@ export default function FoodsList() {
                           type="checkbox"
                           id={`availabilityDesktop-${food.id}`}
                           name="availability"
+                          className="size-4"
                         />
                       </div>
                     </div>
@@ -159,7 +153,11 @@ export default function FoodsList() {
                   </td>
                   {/* Category */}
                   <td className="whitespace-nowrap py-3 px-3">
-                    <p>{food.category}</p>
+                    <div
+                      className={`${food.category === "MainCourse" ? "text-green-500" : "text-yellow-500"}`}
+                    >
+                      {food.category}
+                    </div>
                   </td>
                   {/* Name */}
                   <td className="whitespace-nowrap py-3 px-3">
