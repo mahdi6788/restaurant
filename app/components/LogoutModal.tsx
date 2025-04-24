e9469df4503be9811033d94d5c834a7622e2dedf
mahdi6788 <mjalalimech@gmail.com>
2025-04-24 03:52:47 +0330

    redirect to home page after loggingout

--- app/components/LogoutModal.tsx
+++ app/components/LogoutModal.tsx
@@ -13,7 +13,7 @@ export default function LogoutModal({ isOpen, onClose }: CartModalProps) {
 
   const handleSignOut = async () => {
     await signOut({ redirect: false });
-    redirect("/login");
+    redirect("/");
   };
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
